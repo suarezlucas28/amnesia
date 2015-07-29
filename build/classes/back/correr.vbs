52604bc309961501ea6e04659211883f96076f0c
Lucas Marcelo Suárez <suarezlucas28@gmail.com>
2015-07-28 23:00:56 -0400

    Revert "Fisrt Commit"
    This reverts commit 47b734109242482abf28eb2e9186e0f3b5b5c0e5.

--- build/classes/back/correr.vbs
+++
@@ -1,3 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell") 
-WshShell.Run chr(34) & "C:\amnesia\src\back\respaldo.bat" & Chr(34), 0 
-Set WshShell = Nothing
--- src/back/respaldo.bat
+++
@@ -1,7 +0,0 @@
-FOR /F "TOKENS=1-4 DELIMS=/ " %%F IN ('DATE /T') DO (SET TODAY=%%H-%%G-%%F)
-for /f %%i in ('time /t') do set DATE_TIME=%%i
-for /f %%i in ('echo %date_time::=-%') do set DATE_TIME=%%i
-set BUG=amnesia_%TODAY%@%DATE_TIME%.sql
-
-
-mysqldump  --databases --routines amnesia -u root --single-transaction --password= >>c:\amnesia\src\back\%BUG%
